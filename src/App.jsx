c9c90a0c42dcbeba8190a230728f00cf65e7b738
Jano6elmar <jano6belmar@gmail.com>
2025-09-13 02:38:07 -0300

    Fix: navbar hamburguesa ahora se cierra al hacer clic

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import ArcanosMayores from "./pages/ArcanosMayores";
@@ -5,43 +6,42 @@ import ArcanosMenores from "./pages/ArcanosMenores";
 import Lecturas from "./pages/Lecturas";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
+
   return (
     <Router>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div className="container-fluid">
-          {/* Marca */}
-          <Link className="navbar-brand" to="/">
+          <Link className="navbar-brand" to="/" onClick={closeMenu}>
             Tarot App
           </Link>
 
-          {/* Botón hamburguesa (izquierda al lado del logo) */}
           <button
             className="navbar-toggler"
             type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+            onClick={toggleMenu}
+            aria-expanded={isOpen ? "true" : "false"}
           >
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          {/* Links colapsables (alineados a la derecha) */}
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`} id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
-                <Link className="nav-link" to="/arcanos-mayores">
+                <Link className="nav-link" to="/arcanos-mayores" onClick={closeMenu}>
                   Arcanos Mayores
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/arcanos-menores">
+                <Link className="nav-link" to="/arcanos-menores" onClick={closeMenu}>
                   Arcanos Menores
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/lecturas">
+                <Link className="nav-link" to="/lecturas" onClick={closeMenu}>
                   Lecturas
                 </Link>
               </li>
@@ -50,7 +50,6 @@ function App() {
         </div>
       </nav>
 
-      {/* Margen para que el contenido no quede tapado */}
       <div style={{ marginTop: "80px" }}>
         <Routes>
           <Route path="/" element={<Home />} />
